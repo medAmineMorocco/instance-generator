7e9eb0766af77f3bb503a114d218d82d95d228b9
ammach <Ammach Mohamed Amine>
2022-01-09 22:37:42 +0100

    fix: distance between two words

--- src/com/plugin/utils/StringUtils.java
+++ src/com/plugin/utils/StringUtils.java
@@ -1,16 +1,39 @@
 package com.plugin.utils;
 
+import java.util.Arrays;
+
 public class StringUtils {
 
-    public static int hammingDist(String str1, String str2)
+    public static int hammingDist(String x, String y)
     {
-        int i = 0, count = 0;
-        while (i < str1.length())
-        {
-            if (str1.charAt(i) != str2.charAt(i))
-                count++;
-            i++;
+        int[][] dp = new int[x.length() + 1][y.length() + 1];
+
+        for (int i = 0; i <= x.length(); i++) {
+            for (int j = 0; j <= y.length(); j++) {
+                if (i == 0) {
+                    dp[i][j] = j;
+                }
+                else if (j == 0) {
+                    dp[i][j] = i;
+                }
+                else {
+                    dp[i][j] = min(dp[i - 1][j - 1]
+                                    + costOfSubstitution(x.charAt(i - 1), y.charAt(j - 1)),
+                            dp[i - 1][j] + 1,
+                            dp[i][j - 1] + 1);
+                }
+            }
         }
-        return count;
+
+        return dp[x.length()][y.length()];
+    }
+
+    public static int costOfSubstitution(char a, char b) {
+        return a == b ? 0 : 1;
+    }
+
+    public static int min(int... numbers) {
+        return Arrays.stream(numbers)
+                     .min().orElse(Integer.MAX_VALUE);
     }
 }
